c054a0a069aab97061f7be2ba3504a9e4f6a9221
JulianG <julian.german05@gmail.com>
2020-05-08 13:46:26 +0200

    add 2 exercises

--- glowny.py
+++ glowny.py
@@ -3,21 +3,31 @@ import random
 
 ## Dwie cyfry silni
 
-D = int(input("Podaj liczbe : "))
+"""D = int(input("Podaj liczbe : "))
 a = 1
 for i in range(1, D+1):
     a = a*i
     print(a)
 print(a)
-print(a % 10, "\t", int((a % 100) / 10))
+print(a % 10, "\t", int((a % 100) / 10))"""
 
 ## NWD
 
-a= int(input("Podaj 1 liczbe : "))
+"""a= int(input("Podaj 1 liczbe : "))
 b= int(input("Podaj 2 liczbe : "))
 
 print(a,b)
 
+dzielnik = 1
+dzielniki = []
+
+while dzielnik < a or dzielnik < b:
+    if a % dzielnik == 0 and b % dzielnik == 0:
+        dzielniki.append(dzielnik)
+    dzielnik += 1
+print(dzielniki[dzielniki.__len__()-1])"""
+
+##
 
 
 
